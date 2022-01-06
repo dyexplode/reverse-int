702eb49fe3063774f73ff3a28649b4c49aeb1fd2
Vladimir <admin@ttp.by>
2022-01-06 19:35:32 +0300

    feat: Add function with new syntaxis

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-module.exports = function reverse (n) {
-  const number = Math.abs(n).toString();
-  let res ='';
-  for (let i = number.length; i>0; i--){
-    res = `${res}${number[i-1]}`;   
-  };
-  return res;
-}
+// Old syntaxis
+// module.exports = function reverse (n) {
+//   const number = Math.abs(n).toString();
+//   let res ='';
+//   for (let i = number.length; i>0; i--){
+//     res = `${res}${number[i-1]}`;   
+//   };
+//   return res;
+// }
+
+//New syntaxys
+module.exports = reverse = (n) => Math.abs(n).toString().split('').reverse().join('')
